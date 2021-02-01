b3cbcae26a2c9e8e05bf8ddfb2e2a2d5c545584c
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-01 12:27:56 -0800

    TS in react functional component

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -13,33 +13,52 @@ interface AppStates {
     count:number
 }
 
-class App extends React.Component<AppProps, AppStates>{
-    constructor(props:AppProps){
-        super(props)
-        this.state = {
-            count: 0
-        }
-    }
+// class App extends React.Component<AppProps, AppStates>{
+//     constructor(props:AppProps){
+//         super(props)
+//         this.state = {
+//             count: 0
+//         }
+//     }
   
-    // because both have returned nothing, we used void
-    add = ():void => {
-        this.setState({count: this.state.count + 1})
+//     // because both have returned nothing, we used void
+//     add = ():void => {
+//         this.setState({count: this.state.count + 1})
+//     }
+//     subtract = ():void => {
+//         this.setState({count: this.state.count - 1})
+//     }
+//     render(){
+//         return(
+//             <>
+//             <h1>{this.state.count}</h1>
+//             <div>
+//                 <button onClick={this.add} >increment</button>
+//                 <button onClick={this.subtract}>decrement</button>
+//             </div>
+//             </>
+//         )
+//     }
+// }
+const App = (props : AppProps):JSX.Element => {
+    console.log("hello")
+    const [count , setCount ] = React.useState(0 as number)
+     // because both have returned nothing, we used void
+     const add = ():void => {
+        setCount( count + 1)
     }
-    subtract = ():void => {
-        this.setState({count: this.state.count - 1})
+    const subtract = ():void => {
+        setCount(count - 1)
     }
-    render(){
         return(
             <>
-            <h1>{this.state.count}</h1>
+            <h1>{count}</h1>
             <div>
-                <button onClick={this.add} >increment</button>
-                <button onClick={this.subtract}>decrement</button>
+                <button onClick={add} >increment</button>
+                <button onClick={subtract}>decrement</button>
             </div>
             </>
         )
-    }
 }
-
 
 ReactDOM.render(<App  /> , document.getElementById("root"))
