5fae875f042a4911573981c92837f74650da14f1
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-01 13:05:00 -0800

    setup redux

---
+++ rrts/src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export class App extends React.Component{
+    render():JSX.Element{
+        return (
+            <div>HEllo world</div>
+        )
+    }
+}
--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,64 +1,41 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
+import {App} from "./components/App"
 
 // to rich your component with the data we are dealing with 
 //  we are gonna use an interface to say 'hi component, at the moment,
 // we are using this data and the type of it is this and this'
 
-interface AppProps {
-    color?:string,
-}
-interface AppStates {
-    count:number
-}
 
-// class App extends React.Component<AppProps, AppStates>{
-//     constructor(props:AppProps){
-//         super(props)
-//         this.state = {
-//             count: 0
-//         }
-//     }
-  
-//     // because both have returned nothing, we used void
-//     add = ():void => {
-//         this.setState({count: this.state.count + 1})
-//     }
-//     subtract = ():void => {
-//         this.setState({count: this.state.count - 1})
-//     }
-//     render(){
-//         return(
-//             <>
-//             <h1>{this.state.count}</h1>
-//             <div>
-//                 <button onClick={this.add} >increment</button>
-//                 <button onClick={this.subtract}>decrement</button>
-//             </div>
-//             </>
-//         )
-//     }
-// }
-const App = (props : AppProps):JSX.Element => {
-    console.log("hello")
-    const [count , setCount ] = React.useState(0 as number)
-     // because both have returned nothing, we used void
-     const add = ():void => {
-        setCount( count + 1)
-    }
-    const subtract = ():void => {
-        setCount(count - 1)
-    }
-        return(
-            <>
-            <h1>{count}</h1>
-            <div>
-                <button onClick={add} >increment</button>
-                <button onClick={subtract}>decrement</button>
-            </div>
-            </>
-        )
-}
+// redux
+import {createStore , applyMiddleware} from "redux"
+// some packages have already @types/definition installed with them 
+// but react-redux don't . So , we need to install @types
+import {Provider} from "react-redux"
+import thunk from "redux-thunk"
 
-ReactDOM.render(<App  /> , document.getElementById("root"))
+
+// store
+import {reducers} from "./reducers/index"
+const store = createStore(reducers , applyMiddleware(thunk))
+ 
+
+ReactDOM.render(
+<Provider store={store}>
+    <App  /> 
+</Provider>,
+ document.getElementById("root"))
+// import React from "react"
+// import ReactDOM from "react-dom"
+// import {App} from "./components/App"
+// import {createStore, applyMiddleware} from "redux"
+// import {Provider} from "react-redux"
+// import thunk from "redux-thunk"
+// import {reducers} from "./reducers/index"
+
+// let store = createStore(reducers, applyMiddleware(thunk))
+
+// ReactDOM.render(<Provider store={store}>
+//     <App />
+// </Provider>,
+// document.getElementById("root"))
