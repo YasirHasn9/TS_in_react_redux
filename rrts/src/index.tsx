65f09ecff770bbe2199d8d2928c3aa7021467319
Yasir Hassan <hasanyasir01@gmail.com>
2021-02-01 14:57:41 -0800

    commit changes

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -25,17 +25,3 @@ ReactDOM.render(
     <App  /> 
 </Provider>,
  document.getElementById("root"))
-// import React from "react"
-// import ReactDOM from "react-dom"
-// import {App} from "./components/App"
-// import {createStore, applyMiddleware} from "redux"
-// import {Provider} from "react-redux"
-// import thunk from "redux-thunk"
-// import {reducers} from "./reducers/index"
-
-// let store = createStore(reducers, applyMiddleware(thunk))
-
-// ReactDOM.render(<Provider store={store}>
-//     <App />
-// </Provider>,
-// document.getElementById("root"))
